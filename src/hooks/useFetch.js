b7c47571d9491db30f2e2905cd30dc6a1234f5a0
Naveen Tehrpariya <naveen@internetbusinesssolutionsindia.com>
2024-05-24 18:20:58 +0530

    latest changes

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,23 +1,22 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
+import Api from '../api/Api';
 export default function useFetch(){
 
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
 
-  const fetchData  = (url) => { 
-      axios.get(url).then((res)=>{
-        console.log("data11",res)
-        setIsLoading(false);
-        setData(data);
-        setError(null);
-      }).catch(err => {
-        setIsLoading(false);
-        setError(err.message);
-      });
+  async function fetch(url) {
+    setIsLoading(true);
+    const resp = Api.get(url)
+    resp.then((res)=>{
+      setData(res.data)
+    }).catch((err)=>{
+      console.log(err);
+      setIsLoading(false);
+    });
   }
 
-  return { data, isLoading, error, setData, setIsLoading, fetchData };
+  return { fetch, isLoading, data };
 }
 
